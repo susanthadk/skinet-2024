39771d92dcaf8a909a9ddd6c471a29b78bf0dcd4
Susantha Kumara <dksusantha@outlook.com>
2025-01-13 06:16:07 +0530

    Section 96 completed

--- skinet/client/src/app/core/services/snackbar.service.ts
+++ skinet/client/src/app/core/services/snackbar.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SnackbarService {
+  private snackbar = inject(MatSnackBar);
 
-  constructor() { }
+  error(message: string){
+    this.snackbar.open(message, 'close', {
+      duration: 5000,
+      panelClass: ['snack-error']
+    });
+  }
+
+  success(message: string){
+    this.snackbar.open(message, 'close', {
+      duration: 5000,
+      panelClass: ['snack-success']
+    });
+  }
 }
