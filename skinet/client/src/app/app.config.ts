ce7d9103a56097e7ff448f588da78cd9c4dd751b
Susantha Kumara <dksusantha@outlook.com>
2025-01-13 05:52:42 +0530

    Section 95 completed

--- skinet/client/src/app/app.config.ts
+++ skinet/client/src/app/app.config.ts
@@ -1,15 +1,16 @@
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([errorInterceptor])),
   ],
 };
---
+++ skinet/client/src/app/features/test-error/test-error.component.ts
@@ -0,0 +1,53 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-test-error',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+  ],
+  templateUrl: './test-error.component.html',
+  styleUrl: './test-error.component.scss'
+})
+export class TestErrorComponent {
+  baseUrl = 'http://localhost:5000/api/';
+  private http = inject(HttpClient);
+
+  get404Error(){
+    this.http.get(this.baseUrl + 'buggy/not-found').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    });
+  }
+
+  get400Error(){
+    this.http.get(this.baseUrl + 'buggy/badrequest').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    });
+  }
+
+  get401Error(){
+    this.http.get(this.baseUrl + 'buggy/unauthorized').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    });
+  }
+
+  get500Error(){
+    this.http.get(this.baseUrl + 'buggy/internalerror').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    });
+  }
+
+  get400ValidationError(){
+    this.http.post(this.baseUrl + 'buggy/validationerror', {}).subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    });
+  }
+
+}
